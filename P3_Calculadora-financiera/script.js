b1aacaf6f529e049f450b5eb45745cca4a2a0a7a
islacrur <islacrur@gmail.com>
2023-11-23 14:28:02 +0100

    Update index, starting coding js

--- P3_Calculadora-financiera/script.js
+++ P3_Calculadora-financiera/script.js
@@ -1,1 +1,49 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calc__keys');
 
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+        //Do something
+    }
+})
+
+const key = e.target;
+const action = key.dataset.action;
+
+if(!action) {
+    console.log("number key!");
+}
+
+if (
+    action === 'add' ||
+    action === 'substract' ||
+    action === 'multiply' ||
+    action === 'divide'
+) {
+    console.log('operator key!')
+}
+
+if(action === 'decimal') {
+    console.log("decimal key!");
+}
+
+if(action === 'clear') {
+    console.log("clear key!");
+}
+if(action === 'calculate') {
+    console.log("equal key!");
+}
+
+
+/*
+
+const display = document.querySelector(".calculator___display");
+
+keys.addEventListener('click', e =>{
+    if(e.target.matches('button')) {
+        const action = key.dataset.action
+        const keycontent = key.textContent
+        const displayedNum = display.textContent
+    }
+})
+*/
