cc0c01f9f70e9dc6eea5a80fdbc8cc8943f8c135
islacrur <islacrur@gmail.com>
2023-11-23 15:17:10 +0100

    Blue-orange style and better classes and id

--- P3_Calculadora-financiera/script.js
+++ P3_Calculadora-financiera/script.js
@@ -1,40 +1,6 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calc__keys');
 
-keys.addEventListener('click', e => {
-    if(e.target.matches('button')) {
-        //Do something
-    }
-})
-
-const key = e.target;
-const action = key.dataset.action;
-
-if(!action) {
-    console.log("number key!");
-}
-
-if (
-    action === 'add' ||
-    action === 'substract' ||
-    action === 'multiply' ||
-    action === 'divide'
-) {
-    console.log('operator key!')
-}
-
-if(action === 'decimal') {
-    console.log("decimal key!");
-}
-
-if(action === 'clear') {
-    console.log("clear key!");
-}
-if(action === 'calculate') {
-    console.log("equal key!");
-}
-
-
 /*
 
 const display = document.querySelector(".calculator___display");
