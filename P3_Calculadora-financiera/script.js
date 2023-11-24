16b61f77719d1bfd39bfa401341dd2a6f779d62c
islacrur <islacrur@gmail.com>
2023-11-24 14:38:50 +0100

    fatality

--- P3_Calculadora-financiera/script.js
+++ P3_Calculadora-financiera/script.js
@@ -13,37 +13,3 @@ const action = key.dataset.action;
 if(!action) {
     console.log("number key!");
 }
-
-if (
-    action === 'add' ||
-    action === 'substract' ||
-    action === 'multiply' ||
-    action === 'divide'
-) {
-    console.log('operator key!')
-}
-
-if(action === 'decimal') {
-    console.log("decimal key!");
-}
-
-if(action === 'clear') {
-    console.log("clear key!");
-}
-if(action === 'calculate') {
-    console.log("equal key!");
-}
-
-
-/*
-
-const display = document.querySelector(".calculator___display");
-
-keys.addEventListener('click', e =>{
-    if(e.target.matches('button')) {
-        const action = key.dataset.action
-        const keycontent = key.textContent
-        const displayedNum = display.textContent
-    }
-})
-*/
