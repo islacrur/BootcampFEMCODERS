982ae9c94902f1cf0f4ac6f0940baae43db907a6
islacrur <islacrur@gmail.com>
2023-11-24 11:48:31 +0100

    Functions ok

--- P3_Calculadora-financiera/script.js
+++ P3_Calculadora-financiera/script.js
@@ -3,7 +3,37 @@ const botones = document.querySelectorAll(".btn");
 
 botones.forEach(boton => {
     boton.addEventListener("click", () =>{
-        console.log(boton.textContent)
+        const botonApretado = boton.textContent;
+
+        if(boton.id === "c") {
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if(boton.id === "borrar") {
+            if(pantalla.textContent.length === 1 || pantalla.textContent === "Error") {
+                pantalla.textContent = "0";
+                return;
+            } else {
+                pantalla.textContent = pantalla.textContent.slice(0,-1);
+            } 
+            return;
+        }
+
+        if(boton.id === "igual") {
+            try {
+                pantalla.textContent = eval(pantalla.textContent);
+            } catch {
+                pantalla.textContent = "Error"; 
+            }
+            return;
+        }
+
+        if(pantalla.textContent === "0") {
+            pantalla.textContent = botonApretado;
+        } else {
+        pantalla.textContent += botonApretado;
+        }  
     })
     
 });
