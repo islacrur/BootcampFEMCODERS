77d690843fcc00930226977142be08c4136fa3b5
islacrur <islacrur@gmail.com>
2023-12-22 12:58:26 +0100

    API consumption works correctly

--- API_3_Pokedex/script.js
+++ API_3_Pokedex/script.js
@@ -1,5 +1,5 @@
-const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const poke_container = document.getElementById('poke-container');
+const pokemon_count = 150;
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -15,4 +15,26 @@ const colors = {
 	flying: '#F5F5F5',
 	fighting: '#E6E0D4',
 	normal: '#F5F5F5'
+};
+
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+for (i=1; i<=150; i++) {
+    fetch(URL + i)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 }
+
+
+
+/* 
+<div class="pokemon" style="background-color: rgb(222,253,224);">
+        <div class="img-container">
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur">
+        </div>
+        <div class="info">
+          <span class="number">001</span>
+          <h3 class="name">Bulbasaur</h3>
+          <small class="type">Type: <span>grass</span></small>
+        </div>
+      </div> */
