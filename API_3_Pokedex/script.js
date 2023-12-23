f4d534b8c56b2625f5e0a22d8abfec28a14d8e81
islacrur <islacrur@gmail.com>
2023-12-23 18:56:44 +0100

    Inner HTML, images, ids and types

--- API_3_Pokedex/script.js
+++ API_3_Pokedex/script.js
@@ -22,19 +22,51 @@ let URL = "https://pokeapi.co/api/v2/pokemon/";
 for (i=1; i<=150; i++) {
     fetch(URL + i)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => showPoke(data));
+}
+
+function showPoke(poke) {
+    const createPokemonCard = document.createElement("div");
+    createPokemonCard.classList.add("pokemon");
+    createPokemonCard.innerHTML = 
+    `<div class="img-container">
+    <img src=${poke.sprites.other["official-artwork"].front_default} alt=${poke.name}>
+    </div>
+    <div class="info">
+      <span class="number">#${poke.id}</span>
+      <h3 class="name">${poke.name}</h3>
+      <small class="type">Type: <span>${poke.types[0].type.name}</span></small>
+  </div>`;
+  poke_container.append(createPokemonCard);
+
 }
 
 
 
 /* 
+
 <div class="pokemon" style="background-color: rgb(222,253,224);">
-        <div class="img-container">
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur">
-        </div>
-        <div class="info">
-          <span class="number">001</span>
-          <h3 class="name">Bulbasaur</h3>
-          <small class="type">Type: <span>grass</span></small>
-        </div>
-      </div> */
+    <div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur">
+    </div>
+    <div class="info">
+        <span class="number">001</span>
+        <h3 class="name">Bulbasaur</h3>
+        <small class="type">Type: <span>grass</span></small>
+    </div>
+</div> 
+
+*/
+
+/******************************/
+/******************************/
+
+//Vamos a mostrar nuestros Pokemon en una DIV que se llame "poke-container" para ello creamos una variable con este nombre que traiga del HTML este elemento, por ejempo por id
+
+//Creamos una constante para la URL y creamos un bucle FOR (i=1;1<=150;i++) dentro del cual haremos Fetch de la URL+i(para que después del slash de la URL añada los números de los Pokemon del primero al último) y .then para la response y la data
+
+//  
+
+
+/******************************/
+/******************************/
