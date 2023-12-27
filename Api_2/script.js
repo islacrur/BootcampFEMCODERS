e455c50dc59dc03e60276cdf06c08c2df96a0552
islacrur <islacrur@gmail.com>
2023-12-27 19:17:41 +0100

    Axios installed and gitIgnore created

--- Api_2/script.js
+++ Api_2/script.js
@@ -1,1 +1,5 @@
-const APIURL = 'https://api.github.com/users/'
+const APIURL = 'https://api.github.com/users/';
+
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
